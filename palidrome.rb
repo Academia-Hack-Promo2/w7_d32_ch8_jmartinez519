76e74f92357079695efcdd9dcd98fecbc7dd344f
Jonathan Martinez <jamm@jmartinezm.org>
2015-04-05 08:50:40 -0430

    Agregado validaciones

--- palidrome.rb
+++ palidrome.rb
@@ -8,20 +8,37 @@
 def palindrome(word)
   word_reverse = ""
   word.each_char { |letter| word_reverse = letter + word_reverse }
-  puts word_reverse
   if word == word_reverse
-    puts "La palabra #{word} es palindrome"
+    puts "La palabra #{word} es palindrome" 
   else
     puts "La palabra #{word} no es palindrome"
   end
 end
 
+def is_a_word(word)
+
+  # if word == ""
+  #   puts "Debe escribir alguna palabra para comprobar"
+  if /^[a-z]*$/ === word
+    return true
+  else
+    return false
+  end
+end
 
 
 def main
   print "Introduzca la palabra a comprobar: "
-  word = gets.chomp.to_s
-  palindrome(word)
+  word = gets.chomp.to_s.downcase
+  if word != ""
+    if is_a_word(word)
+      palindrome(word)
+    else
+      puts "Este string no parece una palabra"
+    end
+  else
+    puts "La palabra no puede estar vacia."
+  end
 end
 
 main
